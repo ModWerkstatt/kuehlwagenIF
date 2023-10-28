bcac324449fa2ca67a7e8867024f134c4264795c
Florian Thiel <flth@flth.de>
2023-10-28 13:34:54 +0200

    initial

---
+++ mod.lua
@@ -0,0 +1,27 @@
+﻿function data()
+  return {  
+	info = {
+		minorVersion = 0,
+		severityAdd = "NONE",
+		severityRemove = "WARNING", 
+		name = _("mod_name"),
+		description = _(
+						"mod_desc"
+						),
+		authors = {
+			{
+				name = 'jay_',
+				role = 'CREATOR',
+				text = 'Modell',
+				tfnetId = 28954,
+			},
+					    {
+		        name = "ModWerkstatt",
+		        role = "CREATOR",
+		    },  
+		},
+		tags = { "Europe", "Vehicle", "Freight", "Wagon", "Interfrigo", "DB", "SNCF", "SBB", "FS" },
+		dependencies = {},
+		}, 
+}
+end
---
+++ strings.lua
@@ -0,0 +1,35 @@
+function data()
+return {
+	en = {
+
+		["mod_name"] = "UIC-Standard-1 Cooling Wagon (Ibes)",
+		["mod_desc"] = "The International Union of Railways (UIC) included two refrigerator car types in its proposals of standardized car designs. Standard 1 (St. 1) became the most widely built refrigerator car to this day. The state railroads of Greece, Morocco, Italy, Switzerland, France, the Netherlands and Belgium procured examples of the St. 1. On the German Federal Railroad, they ran as private cars of the Interfrigo company. Meat companies, breweries and grocery chains used this refrigerated car. A total of about 6,500 units were built. In the 1960s and 1970s, there was hardly a freight train without these reliable cars. The ice chambers could be loaded with ice bars by hand via end platforms.",
+		["height"] = "Height",
+		["height_tooltip"] = "Configure height",
+		["jrm_param_offsetx_tooltip"] = "Configure offset",
+		["jrm_param_type_tooltip"] = "Type",
+		["offset x-axis"] = "Offset x-axis",
+		["ground"] = "Ground",
+		["rail"] = "Rail",
+		["vehicle"] = "Vehicle",
+		
+
+		
+	},
+
+	de = {
+
+		["mod_name"] = "UIC-Standard-1 Kühlwagen (Ibes)",
+		["mod_desc"] = "Der internationale Eisenbahnverband UIC nahm in seine Vorschläge von standardisierten Wagenbauarten zwei Kühlwagentypen auf. Der Standard 1 (St. 1) wurde zum bis heute meistgebauten Kühlwagen. Die Staatsbahnen Griechenlands, Marokkos, Italiens, der Schweiz, Frankreichs, der Niederlande und Belgiens beschafften Exemplare des St. 1. Bei der Deutschen Bundesbahn fuhren sie als Privatwagen der Firma Interfrigo. Fleischfirmen, Brauereien und Lebensmittelketten stellten diesen Kühlwagen ein. Insgesamt wurden circa 6.500 Exemplare gebaut. In den 60er- und 70er-Jahren existierte kaum ein Güterzug ohne den zuverlässigen Wagen. Über Endbühnen konnten die Eiskammern von Hand mit Eisstangen bestückt werden.",
+		["height"] = "Höhe",
+		["height_tooltip"] = "Höhe einstellen",
+		["jrm_param_offsetx_tooltip"] = "Versatz einstellen",
+		["jrm_param_type_tooltip"] = "Typ",
+		["offset x-axis"] = "Verschub X-Achse",
+		["ground"] = "Boden",
+		["rail"] = "Gleis",
+		["vehicle"] = "Fahrzeug",
+        
+	},
+}
+end
